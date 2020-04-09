cf1c8da0e79b5044895824ac8fee844ce3a82465
Dom <xblgator@live.com>
2020-04-09 20:37:24 +0900

    Updates

---
+++ precache-manifest.4b087d691af1a926108ec9456add011f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1940fe6c720e585ab989004770ad8f9",
+    "url": "/memorycards/index.html"
+  },
+  {
+    "revision": "bf09db143268c5d52296",
+    "url": "/memorycards/static/css/main.4760a65c.chunk.css"
+  },
+  {
+    "revision": "8215902d5413f3dd35c2",
+    "url": "/memorycards/static/js/2.4b32d394.chunk.js"
+  },
+  {
+    "revision": "16059dd4bfa79142a188c2fe23f59afa",
+    "url": "/memorycards/static/js/2.4b32d394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf09db143268c5d52296",
+    "url": "/memorycards/static/js/main.4442a52f.chunk.js"
+  },
+  {
+    "revision": "28cccb17bf1dbc2ac5cf",
+    "url": "/memorycards/static/js/runtime-main.4faefcf1.js"
+  }
+]);
